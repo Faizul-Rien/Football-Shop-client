d7f02366509f9a518de612dcae2fde9140638fc5
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-09 16:06:29 +0600

    new firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,18 +4,19 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBwDk3whXj9fiIXCAbGmCzekPscje27YEc",
-  authDomain: "football-universe-9cbe7.firebaseapp.com",
-  projectId: "football-universe-9cbe7",
-  storageBucket: "football-universe-9cbe7.appspot.com",
-  messagingSenderId: "22228915726",
-  appId: "1:22228915726:web:9faf975a712716cddd8e5f"
+  apiKey: "AIzaSyAOrvKFZqIFWgdRO9sjF82xeAY7j4DjwUE",
+  authDomain: "football-server-e013e.firebaseapp.com",
+  projectId: "football-server-e013e",
+  storageBucket: "football-server-e013e.appspot.com",
+  messagingSenderId: "1001610055817",
+  appId: "1:1001610055817:web:128c4bedbcd894672bf658"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+
 
 const auth = getAuth(app);
 
